954fd62087c5cedee4a158476ea23e083bacedb7
seokyeong-han <tjrdud3347@gmail.com>
2025-07-01 21:43:29 +0900

    프로젝트 기본 설정 업데이트

---
+++ spirng/src/test/java/com/example/spirng/SpirngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spirng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpirngApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
