ea9bd76bcd43b684758efa6aaa8d80d12860026a
seokyeong-han <tjrdud3347@gmail.com>
2025-07-15 22:25:41 +0900

    [repository] 주석 업데이트 레파지토리 구현체

--- spirng/src/main/java/com/example/spirng/repository/MemoryMemberRepository.java
+++ spirng/src/main/java/com/example/spirng/repository/MemoryMemberRepository.java
@@ -6,6 +6,7 @@ import java.util.*;
 
 /**
  * 동시성 문제가 고려되어 있지 않음, 실무에서는 ConcurrentHashMap, AtomicLong 사용 고려
+ * 리포지토리를 인터페이스로 추상화하면, 실제 데이터 저장 기술(DB, JPA 등)이 나중에 결정되어도 인터페이스를 구현하는 다른 클래스로 쉽게 교체할 수 있습니다. 초기엔 메모리 구현체를 활용하기도 합니다.
  */
 public class MemoryMemberRepository implements MemberRepository{
         private static Map<Long, Member> store = new HashMap<>();
